a2f81574f987fff5c37c30a3416046f73b6112a0
Nikhil Tilwalli <ntilwalli@gmail.com>
2016-02-29 13:56:28 -0500

    rebuild

--- dist/src/cycle-mapdom.js
+++ dist/src/cycle-mapdom.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.makeMapDOMDriver = exports.g_mapElementRegistry = undefined;
+exports.g_registeredElement = exports.makeMapDOMDriver = undefined;
 
 var _slicedToArray = (function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; })(); // Importing 'rx-dom' imports 'rx' under the hood, so no need to
 
@@ -46,7 +46,7 @@ var VDOM = {
 var g_MBAccessToken = undefined;
 var g_MBMapOptions = undefined;
 
-var g_mapElementRegistry = exports.g_mapElementRegistry = {};
+var g_registeredElement = undefined;
 
 function makeEmptyMapVDOMNode(options) {
   return new _virtualDom.VNode('map', { options: options });
@@ -82,199 +82,27 @@ function makeDiffAndPatchToElement$() {
     /* eslint-disable */
 
     var anchorId = getAnchorIdFromVTree(newVTree);
-    var proxyElement = g_mapElementRegistry[anchorId];
+    var mapDOM = (0, _virtualMapdom.getMapFromElement)(g_registeredElement);
     var diffInfo = VDOM.diff(oldVTree, newVTree);
 
     // console.log("Diff old vs new VDOM tree...")
     // console.log(diffInfo)
 
-    var rootElem = VDOM.patch(proxyElement.mapDOM, diffInfo, { render: _virtualMapdom.render, patch: _virtualMapdom.patchRecursive });
+    var rootElem = VDOM.patch(mapDOM, diffInfo, { render: _virtualMapdom.render, patch: _virtualMapdom.patchRecursive });
 
     /* eslint-enable */
 
-    return _rxDom2.default.Observable.just(proxyElement.mapDOM);
+    return _rxDom2.default.Observable.just(mapDOM);
   };
-}
-
-function bufferWhile(source, selector, isRegulationMessage) {
-  return _rxDom2.default.Observable.create(function (observer) {
-
-    var buffer = [];
-    source.subscribe(function (x) {
-      var selectorVal = selector(x);
-      var regulator = isRegulationMessage(x);
-      if (selectorVal === false) {
-        if (!regulator) {
-          //console.log("Buffering value...")
-          //console.log(x)
-          buffer.push(x);
-        }
-        // else {
-        //   console.log("Skipping extraneous regulation message...")
-        // }
-      } else {
-          //console.log("Allowing buffered values release...")
-          var bufferLen = buffer.length;
-          if (bufferLen > 0) {
-            //console.log("Buffered values exist...")
-            var swapBuffer = [];
-            var i = -1;
-            while (++i < bufferLen) {
-              var val = buffer[i];
-              // There could be multiple unrelated streams
-              // aggregated in buffer where predicate is true for one
-              // stream but not for others, need to recheck each
-              // buffered entry before releasing
-              if (selector(val)) {
-                //console.log("Releasing buffered value: " + JSON.stringify(val))
-                observer.onNext(val);
-              } else {
-                //console.log("Not releasing buffered value: " + JSON.stringify(val))
-                swapBuffer.push(val);
-              }
-            }
-
-            buffer = swapBuffer;
-          }
-
-          if (!regulator) {
-            //console.log("Passing through trigger message...")
-            observer.onNext(x);
-          } else {
-            //console.log("Filtering out regulation message for onNext call...")
-          }
-        }
-    }, observer.onError.bind(observer), observer.onCompleted.bind(observer));
-  });
-}
-
-function vdomRegulator(x) {
-  return x instanceof MutationRecord;
 }
 
 function getAnchorIdFromVTree(vtree) {
   return vtree.properties.anchorId;
 }
 
-function makeSelectorFunction(isRegulationMessage) {
-  var rma = g_mapElementRegistry;
-
-  function getDOMElement(x) {
-    //console.log("Printing observation root...")
-    //console.dir(observationRoot)
-    return document.getElementById(x);
-  }
-
-  // This function will be called whenever a node is added or removed from the
-  // DOM or when a new map state is sent
-  return function selectorFunction(x) {
-
-    //console.log("Calling selectorFunction...")
-
-    // If it's not a regulator message then it's a VDOM message and
-    // the root vdom element should always have an anchorId propery
-    // (to-be-attached or attached map)
-    if (!isRegulationMessage(x)) {
-      //console.log("VTree message sent to selector...")
-      //console.log(x)
-      // Not a regulator message
-      var anchorId = getAnchorIdFromVTree(x);
-      if (!anchorId) {
-        // For some reason console.error is not called from tape...
-        console.log('WARNING: No anchorId given in VTree. AnchorId is required for map to be created.');
-        // Should never throw an exception from a subscribe function so commenting, also because
-        // exception is not being passed up all the way and is causing tape/testling to hang when thrown.
-        // That should not happen but I don't know which layer is the issue, RxJS, testling or tap... don't know which,
-        // but I'm pretty sure it shouldn't cause a hang... leaving here to investigate later...
-        //throw new Error("No anchorId in sent VMapDOM node.")
-      }
-
-      // We can only operate on a map that is attached to a valid DOM element.
-      // There could be a period of time where we start sending map state
-      // for a map that is anchored to an element that is not created in the
-      // DOM yet.
-
-      // Check if we've seen this anchorId before, if not register it while
-      // initializing the value (boolean indicating if anchor represents a
-      // valid DOM element, i.e. element that is attached to DOM) to false
-      var firstTime = false;
-      if (!rma.hasOwnProperty(anchorId)) {
-        //console.log("Registering record for anchor: " + anchorId)
-        rma[anchorId] = false;
-        firstTime = true;
-      }
-
-      // Check the registeredMapAnchor state, if true, then element is
-      // registered and map operations can commence
-      if (rma[anchorId] === false) {
-        // Check if the anchorId is attached to the DOM (i.e. DOM change
-        // happened before the anchorId was registered so a test against
-        // the registeredMapAnchors would have failedwhen the addedNode message
-        // came)
-        var domEl = getDOMElement(anchorId);
-        if (domEl) {
-          if (firstTime) {
-            //console.log("Anchor has already been attached, returning true...")
-          } else {
-              console.log("Dunno if this else statement is ever called...");
-              console.log("Anchor is registered, was not attached but now is, returning true...");
-            }
-
-          (0, _virtualMapdom.createMapOnElement)(domEl, g_MBAccessToken, makeEmptyMapVDOMNode(g_MBMapOptions));
-          //console.dir(domEl)
-          rma[anchorId] = domEl;
-          return true;
-        } else {
-          // This means the anchorId is registered and the element is not yet
-          // valid and we should not allow it to pass
-          //console.log("anchorId is registered but not attached, returning false...")
-          return false;
-        }
-      } else {
-        //console.log("anchorId is registered and attached, returning true...")
-        return true;
-      }
-    } else {
-      //console.log("Regulation message sent to selector...")
-      //console.log(x)
-      var anyAdded = false;
-      // The message is from the MutationObserver, check all the keys in the
-      // registeredMutationObserver. If a key exists then test if the element
-      // exists in the DOM and set it's DOM state appropriately
-      for (var key in rma) {
-        var inDOM = getDOMElement(key);
-        //let testVal = inDOM ? "true" : "false"
-        //console.log("Testing key: " + key + ", " + testVal )
-        if (inDOM) {
-          // If it's in the DOM but the registration is false, set it
-          // to true, and return a message indicating which key was added
-          if (rma[key] === false) {
-            console.log("Transition made: " + key + ", added");
-            //console.log("Adding map element registry...")
-            (0, _virtualMapdom.createMapOnElement)(inDOM, g_MBAccessToken, makeEmptyMapVDOMNode(g_MBMapOptions));
-            rma[key] = inDOM;
-            anyAdded = true;
-          }
-        } else {
-          if (rma[key] !== false) {
-            console.log("Transition made: " + key + ", removed");
-            //console.log("Removing key from map element registry...")
-            delete rma[key];
-          }
-        }
-      }
-
-      // If any relevant elements were added to the DOM then send true
-      // signal
-      return anyAdded;
-    }
-  };
-}
-
 function makeRegulatedRawRootElem$(vtree$) {
 
   var g_registeredAnchorId = undefined;
-  var g_registeredElement = undefined;
   var sharedVTree$ = vtree$.shareReplay(1);
   var anchorRegistration$ = sharedVTree$.do(function (vtree) {
     g_registeredAnchorId = getAnchorIdFromVTree(vtree);
@@ -290,12 +118,13 @@ function makeRegulatedRawRootElem$(vtree$) {
     return g_registeredAnchorId && document.getElementById(g_registeredAnchorId);
   }).distinctUntilChanged().filter(function (element) {
     if (element) {
-      g_registeredElement = element;
+      exports.g_registeredElement = g_registeredElement = element;
       (0, _virtualMapdom.createMapOnElement)(g_registeredElement, g_MBAccessToken, makeEmptyMapVDOMNode(g_MBMapOptions));
       return true;
     } else {
       if (g_registeredElement) {
         (0, _virtualMapdom.removeMapFromElement)(g_registeredElement);
+        exports.g_registeredElement = g_registeredElement = undefined;
       }
       return false;
     }
@@ -416,3 +245,4 @@ function makeMapDOMDriver(accessToken, options) {
 }
 
 exports.makeMapDOMDriver = makeMapDOMDriver;
+exports.g_registeredElement = g_registeredElement;
--- dist/test/main.js
+++ dist/test/main.js
@@ -39,40 +39,11 @@ function _typeof(obj) { return obj && typeof Symbol !== "undefined" && obj.const
 
   setTimeout(function () {
     t.ok(rootEl.mapDOM, "should have valid mapDOM property on given element");
-    t.end();
-  }, 300);
-});
-
-// This is dependent test on the previous one since a map is already attached to
-// the document and has not been removed
-(0, _tapeCatch2.default)("Allow two map streams at same time and removes anchor from registry when root element removed", function (t) {
-
-  var rootEl = document.createElement("div");
-
-  rootEl.setAttribute("id", "testId2");
-
-  var testVMaps = [new _virtualDom.VNode('map', { anchorId: "testId2", centerZoom: { center: [4, 5], zoom: 5 } }), new _virtualDom.VNode('map', { anchorId: "testId2", centerZoom: { center: [4, 5], zoom: 5 } }, [new _virtualDom.VNode('tileLayer', { tile: "testTile", attributes: { id: "testTile2" } })])];
-
-  var map$ = _rxDom2.default.Observable.interval(100).take(2).map(function (x) {
-    return testVMaps[x];
-  });
-
-  var outFunc = (0, _cycleMapdom.makeMapDOMDriver)("pk.eyJ1IjoibXJyZWRlYXJzIiwiYSI6IjQtVVRTZkEifQ.ef_cKBTmj8rSr7VypppZdg");
-  t.equals(typeof outFunc === 'undefined' ? 'undefined' : _typeof(outFunc), 'function', "should output a function");
-  var outVal = outFunc(map$);
-  t.ok(outVal.select && outVal.dispose, "should output object with valid select and dispose properties");
-
-  setTimeout(function () {
-    var bodyEl = document.body.appendChild(rootEl);
-  }, 150);
-
-  setTimeout(function () {
-    t.ok(rootEl.mapDOM, "should have valid mapDOM property on given element");
     document.body.removeChild(rootEl);
   }, 300);
 
   setTimeout(function () {
-    t.notOk(_cycleMapdom.g_mapElementRegistry.hasOwnProperty("testId2"), "anchor should not be registered after root element removal");
+    t.notOk(_cycleMapdom.g_registeredElement, "anchor should not be registered after root element removal");
     t.end();
   }, 2000);
 });
@@ -90,14 +61,11 @@ function _typeof(obj) { return obj && typeof Symbol !== "undefined" && obj.const
   });
 
   var outFunc = (0, _cycleMapdom.makeMapDOMDriver)('pk.eyJ1IjoibXJyZWRlYXJzIiwiYSI6IjQtVVRTZkEifQ.ef_cKBTmj8rSr7VypppZdg');
-  var outVal = outFunc(map$.do(function (x) {
-    return console.log('Hidy ho');
-  }));
+  var outVal = outFunc(map$);
   t.equal(_typeof(outVal.select), 'function', "makeMapDOMDriver should return object with select property that is a function");
   var elem$ = outVal.select("#testTile3").observable;
   t.ok(elem$.subscribe, "elem$ should have subscribe function");
   elem$.doOnNext(function (x) {
-    console.log('Hello');
     x.forEach(function (y) {
       t.equal(y.tagName, "TILELAYER", "selected element should be tileLayer");
       t.ok(y.instance, "element should have in attached instance property");
